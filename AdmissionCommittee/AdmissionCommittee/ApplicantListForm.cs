f6fc30634c016c5241d6400e242f0a416b1f7431
prtsie <yonhsup@mail.ru>
2024-04-11 23:51:41 +0300

    использование класса с ограничениями

--- AdmissionCommittee/AdmissionCommittee/ApplicantListForm.cs
+++ AdmissionCommittee/AdmissionCommittee/ApplicantListForm.cs
@@ -1,3 +1,4 @@
+using AdmissionCommittee.Helpers;
 using AdmissionCommittee.Models;
 
 namespace AdmissionCommittee
@@ -21,14 +22,12 @@ namespace AdmissionCommittee
             var formOfEducationValues = Enum.GetValues<FormOfEducation>();
             var genderValues = Enum.GetValues<Gender>();
             var now = DateTime.Now;
-            var minAge = 18;
-            var maxAge = 21;
             string[] names = ["Алёша", "Вася", "Иннокентий", "Олег", "Доброгей", "Даздраперма", "Наруто"];
             string[] surnames = ["Иванов", "Каерчывпак", "Абдулгаджиев"];
             string[] patronymics = ["Магомедович", "Валентинович", "Евсеевич", "Аыуыаеич"];
             for (var i = 0; i < count; i++)
             {
-                var year = random.Next(now.Year - maxAge, now.Year - minAge + 1);
+                var year = random.Next(now.Year - ApplicantConstraints.MaxAge, now.Year - ApplicantConstraints.MinAge + 1);
                 var month = random.Next(1, 12 + 1);
                 var day = random.Next(1, DateTime.DaysInMonth(year, month));
                 var applicant = new Applicant
