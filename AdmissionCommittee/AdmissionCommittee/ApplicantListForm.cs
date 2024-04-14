9c4841be86f2327de2dfcb5b30f626c695869807
prtsie <yonhsup@mail.ru>
2024-04-14 12:02:50 +0300

    Генерация валидной даты

--- AdmissionCommittee/AdmissionCommittee/ApplicantListForm.cs
+++ AdmissionCommittee/AdmissionCommittee/ApplicantListForm.cs
@@ -29,14 +29,33 @@ namespace AdmissionCommittee
             var formOfEducationValues = Enum.GetValues<FormOfEducation>();
             var genderValues = Enum.GetValues<Gender>();
             var now = DateTime.Now;
+            var minYear = now.Year - ApplicantConstraints.MaxAge;
+            var maxYear = now.Year - ApplicantConstraints.MinAge;
             string[] names = ["Алёша", "Вася", "Иннокентий", "Олег", "Доброгей", "Даздраперма", "Наруто"];
             string[] surnames = ["Иванов", "Каерчывпак", "Абдулгаджиев"];
             string?[] patronymics = ["Магомедович", "Валентинович", "Евсеевич", "Аыуыаеич", null];
             for (var i = 0; i < count; i++)
             {
-                var year = random.Next(now.Year - ApplicantConstraints.MaxAge, now.Year - ApplicantConstraints.MinAge + 1);
-                var month = random.Next(1, 12 + 1);
-                var day = random.Next(1, DateTime.DaysInMonth(year, month));
+                var year = random.Next(minYear, maxYear + 1);
+                int month;
+                int day;
+                if (year == minYear)
+                {
+                    month = random.Next(now.Month, 12 + 1);
+                    var minDay = month == now.Month ? now.Day : 1;
+                    day = random.Next(minDay, DateTime.DaysInMonth(year, month) + 1);
+                }
+                else if (year == maxYear)
+                {
+                    month = random.Next(1, now.Month + 1);
+                    var maxDay = month == now.Month ? now.Day : DateTime.DaysInMonth(year, month);
+                    day = random.Next(1, maxDay);
+                }
+                else
+                {
+                    month = random.Next(1, 12 + 1);
+                    day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+                }
                 var applicant = new Applicant
                 {
                     Name = names[random.Next(names.Length)],
