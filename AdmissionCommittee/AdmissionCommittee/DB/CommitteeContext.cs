ff815e9e88fd6aff32e87ecfd3ced063e142b3c8
prtsie <157487565+prtsie@users.noreply.github.com>
2024-05-24 22:22:22 +0300

    Database (#10)
    * Форма с таблицей
    
    * модель
    
    * Отображение данных из источника в форме
    
    * Класс с ограничениями для полей абитуриента и id для абитуриентов
    
    * Нормальные названия переменных для ограничения возраста
    
    * использование класса с ограничениями
    
    * Генерация непустого гуида
    
    * Кнопка удаления
    
    * Метод расширения
    
    * Переделки для bindable component
    
    * Использование классного метода для привязки
    
    * Форма
    
    * Наследование ICloneable для Applicant
    
    * Исправил ошибки
    
    * Модификаторы public вместо internal
    
    * Использование интерфейса IBindableComponent вместо класса
    
    * Форма с логикой
    
    * Форма с логикой
    
    * Починены комбобоксы
    
    * BindableStruct вместо BindableBool, рабочие комбобоксы
    
    * Валидация
    
    * Валидация (не работает как надо)
    
    * Поменяно обратно на Control вместо IBindableComponent
    
    * errorProvider только для тех полей, где это надо
    
    * Возможность передавать члены, допускающие null, без варнингов
    
    * События вместо привязки кнопок, удаление IsSelected и класса для его привязки
    
    * параметры с маленькой буквы в лямбдах
    
    * Валидация даты
    
    * Валидация всех полей, а не только required
    
    * Вывод количества абитурентов с суммой баллов больше 150
    
    * Вставка добавленных абитуриентов в начало списка
    
    * Генерация абитуриентов без отчества
    
    * добавлены summary
    
    * Уборка варнингов и сообщений
    
    * Уборка варнингов и сообщений
    
    * Уборка варнингов и сообщений
    
    * summary
    
    * Правильная валидация дат (наверное)
    
    * Генерация валидной даты
    
    * Правильная валидация дат (наверное)
    
    * Правильная валидация дат (наверное)
    
    * Правильное условие подсчета абитуриентов по баллам
    
    * Правильное условие подсчета абитуриентов по баллам
    
    * Поправлены иероглифы
    
    * Точку с запятой забыл
    
    * Нормальная работа валидации
    
    * Русские комбобоксы (кроме выбранного элемента)
    
    * Русские заголовки таблицы
    
    * Русская таблица
    
    * Комментарии
    
    * Убрана лишняя строка
    
    * АВтоматическая ширина колонок
    
    * Убран лишний проект
    
    * Исправления
    
    * Календарь рядом с датой
    
    * Сохранение в базу данных
    
    * починки конфликтов
    
    * починка починки конфликтов
    
    * удаление по-другому
    
    * удаление по-другому
    
    * Исправления

---
+++ AdmissionCommittee/AdmissionCommittee/DB/CommitteeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdmissionCommittee.Models;
+
+namespace AdmissionCommittee.DB
+{
+    internal sealed class CommitteeContext : DbContext
+    {
+        public DbSet<Applicant> Applicants { get; set; } = null!;
+    }
+}
